5cf8bff477dbf3f0430684628812fd421e005926
Time6628 <pturchan@yahoo.com>
2016-05-06 18:49:03 -0400

    CraftStatistic :heavy_check_mark:

--- src/main/java/org/bukkit/craftbukkit/v1_8_R3/CraftStatistic.java
+++ src/main/java/org/bukkit/craftbukkit/v1_8_R3/CraftStatistic.java
@@ -4,11 +4,16 @@ import com.google.common.base.CaseFormat;
 import com.google.common.collect.BiMap;
 import com.google.common.collect.ImmutableBiMap;
 import com.google.common.collect.ImmutableMap;
+import net.minecraft.block.Block;
+import net.minecraft.item.Item;
 import net.minecraft.server.v1_8_R3.Block;
 import net.minecraft.server.v1_8_R3.EntityTypes;
 import net.minecraft.server.v1_8_R3.Item;
 import net.minecraft.server.v1_8_R3.MinecraftKey;
 import net.minecraft.server.v1_8_R3.StatisticList;
+import net.minecraft.stats.StatBase;
+import net.minecraft.stats.StatList;
+import net.minecraft.stats.StatisticsFile;
 import org.bukkit.Achievement;
 import org.bukkit.Material;
 import org.bukkit.Statistic;
@@ -55,16 +60,16 @@ public class CraftStatistic {
         achievements = achievementBuilder.build();
     }
 
-    public static Achievement getBukkitAchievement(net.minecraft.server.v1_8_R3.Achievement achievement) {
-        return getBukkitAchievementByName(achievement.name);
+    public static Achievement getBukkitAchievement(net.minecraft.stats.Achievement achievement) {
+        return getBukkitAchievementByName(achievement.getStatName().toString());
     }
 
     public static Achievement getBukkitAchievementByName(String name) {
         return (Achievement) CraftStatistic.achievements.get(name);
     }
 
-    public static Statistic getBukkitStatistic(net.minecraft.server.v1_8_R3.Statistic statistic) {
-        return getBukkitStatisticByName(statistic.name);
+    public static Statistic getBukkitStatistic(StatBase statistic) {
+        return getBukkitStatisticByName(statistic.getStatName().toString());
     }
 
     public static Statistic getBukkitStatisticByName(String name) {
@@ -94,47 +99,47 @@ public class CraftStatistic {
 
         return (Statistic) CraftStatistic.statistics.get(name);
     }
-
-    public static net.minecraft.server.v1_8_R3.Statistic getNMSStatistic(Statistic statistic) {
-        return StatisticList.getStatistic((String) CraftStatistic.statistics.inverse().get(statistic));
+    //TODO: rework this
+    public static StatBase getNMSStatistic(Statistic statistic) {
+        return StatList.getOneShotStat((String) CraftStatistic.statistics.inverse().get(statistic));
     }
 
-    public static net.minecraft.server.v1_8_R3.Achievement getNMSAchievement(Achievement achievement) {
-        return (net.minecraft.server.v1_8_R3.Achievement) StatisticList.getStatistic((String) CraftStatistic.achievements.inverse().get(achievement));
+    public static net.minecraft.stats.Achievement getNMSAchievement(Achievement achievement) {
+        return (net.minecraft.stats.Achievement) StatList.getOneShotStat((String) CraftStatistic.achievements.inverse().get(achievement));
     }
 
-    public static net.minecraft.server.v1_8_R3.Statistic getMaterialStatistic(Statistic stat, Material material) {
+    public static StatBase getMaterialStatistic(Statistic stat, Material material) {
         try {
-            return stat == Statistic.MINE_BLOCK ? StatisticList.MINE_BLOCK_COUNT[material.getId()] : (stat == Statistic.CRAFT_ITEM ? StatisticList.CRAFT_BLOCK_COUNT[material.getId()] : (stat == Statistic.USE_ITEM ? StatisticList.USE_ITEM_COUNT[material.getId()] : (stat == Statistic.BREAK_ITEM ? StatisticList.BREAK_ITEM_COUNT[material.getId()] : null)));
+            return stat == Statistic.MINE_BLOCK ? StatList.mineBlockStatArray[material.getId()] : (stat == Statistic.CRAFT_ITEM ? StatList.objectCraftStats[material.getId()] : (stat == Statistic.USE_ITEM ? StatList.objectUseStats[material.getId()] : (stat == Statistic.BREAK_ITEM ? StatList.objectBreakStats[material.getId()] : null)));
         } catch (ArrayIndexOutOfBoundsException arrayindexoutofboundsexception) {
             return null;
         }
     }
-
-    public static net.minecraft.server.v1_8_R3.Statistic getEntityStatistic(Statistic stat, EntityType entity) {
+    //TODO: Rework this
+    public static StatBase getEntityStatistic(Statistic stat, EntityType entity) {
         EntityTypes.MonsterEggInfo monsteregginfo = (EntityTypes.MonsterEggInfo) EntityTypes.eggInfo.get(Integer.valueOf(entity.getTypeId()));
 
         return monsteregginfo != null ? monsteregginfo.killEntityStatistic : null;
     }
 
-    public static EntityType getEntityTypeFromStatistic(net.minecraft.server.v1_8_R3.Statistic statistic) {
-        String statisticString = statistic.name;
+    public static EntityType getEntityTypeFromStatistic(StatBase statistic) {
+        String statisticString = statistic.getStatName().toString();
 
         return EntityType.fromName(statisticString.substring(statisticString.lastIndexOf(".") + 1));
     }
 
-    public static Material getMaterialFromStatistic(net.minecraft.server.v1_8_R3.Statistic statistic) {
-        String statisticString = statistic.name;
+    public static Material getMaterialFromStatistic(StatBase statistic) {
+        String statisticString = statistic.getStatName().toString();
         String val = statisticString.substring(statisticString.lastIndexOf(".") + 1);
-        Item item = (Item) Item.REGISTRY.get(new MinecraftKey(val));
+        Item item = (Item) Item.itemRegistry.getObject(new MinecraftKey(val));//TODO: rework this.
 
         if (item != null) {
-            return Material.getMaterial(Item.getId(item));
+            return Material.getMaterial(Item.getIdFromItem(item));
         } else {
-            Block block = (Block) Block.REGISTRY.get(new MinecraftKey(val));
+            Block block = (Block) Block.blockRegistry.getObject(new MinecraftKey(val));//TODO: Rework this.
 
             if (block != null) {
-                return Material.getMaterial(Block.getId(block));
+                return Material.getMaterial(Block.getIdFromBlock(block));
             } else {
                 try {
                     return Material.getMaterial(Integer.parseInt(val));
